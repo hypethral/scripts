f7d7e08ebc1df5874df0af1ccc3ddc4652cc0585
hypethral <hypethral1@gmail.com>
2023-06-28 13:07:37 +0800

    removed

--- ETH-flexpool.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./t-rex -a ethash -o stratum+ssl://eth-sg.flexpool.io:5555 -u 0x6b723f48559006ae124dccfef9A04386b95356Bc -p x -w rig0
--- wordlist_to_md5.sh
+++
@@ -1,3 +0,0 @@
-#!bin/sh
-
-for i in $(cat rockyou.txt); do echo -n "$i" | md5sum | tr -d " -" >> hashes; done
